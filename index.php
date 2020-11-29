51cd3664ad8f8cebb0940c256a653f9193d80151
Michael Orzel <michaelorzeld@gmail.com>
2020-11-29 16:29:21 -0500

    created netplay.php

--- index.php
+++ index.php
@@ -23,15 +23,16 @@
 
 </div>
 
-<div class="logo">
+<div class="banner">
 <!--
 <picture>
 	<source srcset="" type="image/webp">
 	<source srcset="" type="image/png">
   	<img src="" alt="">
 </picture>
+
+<img src="https://lh3.googleusercontent.com/oCAxwvibn0y027sU0eex9HVPYJ4AeYGoxwPhJFC6D6LZEifL1UxnApbw0ej7mm2CG41GzDGvUf2JLNI2ax1hdZcX-b-h6L5rTzKXZCfwGp63r55Ih_l67No80SGdZASNJJexD13Jwg=w420-h183-p-k" />
 -->
-	<img src="https://lh3.googleusercontent.com/oCAxwvibn0y027sU0eex9HVPYJ4AeYGoxwPhJFC6D6LZEifL1UxnApbw0ej7mm2CG41GzDGvUf2JLNI2ax1hdZcX-b-h6L5rTzKXZCfwGp63r55Ih_l67No80SGdZASNJJexD13Jwg=w420-h183-p-k" />
 </div>
 </header>
 
@@ -54,7 +55,9 @@
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
     	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-    	<span>
+    	<a href="netplay.php" class="login">Netplay</a>
+		<hr>
+		<span>
 			<a href="index.php?logout='1'" class="login">logout</a>
 		</span>
     <?php endif ?>
